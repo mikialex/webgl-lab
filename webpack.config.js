67076d2429194c6fa4e186da3a4ba4443c8440d6
MikiAlex <18516340862@163.com>
2017-06-05 00:40:21 +0800

    build basic env

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+var TEM_PATH = path.resolve(ROOT_PATH, 'templates');
+
+module.exports = {
+  entry: {
+    app: path.resolve(APP_PATH, 'index.js'),
+    // mobile: path.resolve(APP_PATH, 'mobile.js'),
+    // vendors: []
+  },
+  output: {
+    path: BUILD_PATH,
+    filename: '[name].js'
+  },
+  resolve: {
+    alias: {
+    }
+  },
+  //enable dev source map
+  devtool: 'eval-source-map',
+  //enable dev server
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true,
+  },
+  module: {
+    // preLoaders: [
+    //   {
+    //     test: /\.jsx?$/,
+    //     include: APP_PATH,
+    //     loader: "jshint-loader"
+    //   }
+    // ],
+    // loaders: [
+    //   {
+    //     test: /\.jsx?$/,
+    //     loader: 'babel',
+    //     include: APP_PATH,
+    //     query: {
+    //       presets: ['es2015']
+    //     }
+    //   },
+    //   {
+    //     test: /\.scss$/,
+    //     loaders: ['style', 'css?sourceMap', 'sass?sourceMap'],
+    //     include: APP_PATH
+    //   },
+    //   {
+    //     test: /\.(png|jpg)$/,
+    //     loader: 'url?limit=40000'
+    //   }
+    // ]
+  },
+
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'Hello World app',
+      template: path.resolve(TEM_PATH, 'index.html'),
+      filename: 'index.html',
+      chunks: ['app', 'vendors'],
+      inject: 'body'
+    }),
+    /*new HtmlwebpackPlugin({
+      title: 'Hello Mobile app',
+      template: path.resolve(TEM_PATH, 'mobile.html'),
+      filename: 'mobile.html'
+    }),*/
+  ]
+};
